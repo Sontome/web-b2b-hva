e526f51cedcd9c91f5319e1cdaee55a07cb442e3
Sontome <93179211+Sontome@users.noreply.github.com>
2026-05-30 15:46:27 +0700

    Create changeTicket.ts

---
+++ src/types/changeTicket.ts
@@ -0,0 +1,61 @@
+export interface Segment {
+  seg_no: number;
+  flight_no?: string;
+  flight_number?: string;
+  booking_class?: string;
+  from?: string;
+  to?: string;
+  dep_date?: string;
+  dep_time?: string;
+  arr_time?: string;
+  status?: string;
+  [key: string]: unknown;
+}
+
+export interface PreChangeResponse {
+  seg: Segment[];
+  pnr?: string;
+  [key: string]: unknown;
+}
+
+export interface ChangePrice {
+  penalty_total?: number | string;
+  GRD_TOTAL?: number | string;
+  total_new?: number | string;
+  [key: string]: unknown;
+}
+
+export interface ChangeTicketResponse {
+  status: string;
+  search_command?: string;
+  seg_new?: Segment[];
+  new_price?: ChangePrice;
+  message?: string;
+  namelist?: string[];
+  [key: string]: unknown;
+}
+
+export interface ChangePnrRequest {
+  pnr: string;
+  seg_del: string;
+  dep: string;
+  arr: string;
+  depdate: string;
+  deptime: string;
+  deptimedone: string;
+  arrdate?: string;
+  arrtime?: string;
+  arrtimedone?: string;
+}
+
+export interface FlightContext {
+  dep: string;
+  arr: string;
+  depdate: string;
+  deptime: string;
+  deptimedone: string;
+  arrdate?: string;
+  arrtime?: string;
+  arrtimedone?: string;
+  isRoundTrip: boolean;
+}
